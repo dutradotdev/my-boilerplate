13dbbf8e3e9601e1cdd19acc18de1d496c94accb
LUCAS DUTRA <lucas.dutra@midway.com.br>
2021-04-26 19:29:09 -0300

    feat: integração com storybook

---
+++ .storybook/main.js
@@ -0,0 +1,10 @@
+module.exports = {
+  "stories": [
+    "../src/components/**/stories.tsx"
+  ],
+  "addons": [
+    "@storybook/addon-actions",
+    "@storybook/addon-links",
+    "@storybook/addon-knobs",
+  ]
+}
---
+++ src/components/Main/stories.tsx
@@ -0,0 +1,16 @@
+import { Story, Meta } from '@storybook/react/types-6-0'
+import { withKnobs, text } from '@storybook/addon-knobs'
+import Main from '.'
+
+export default {
+  title: 'Main',
+  component: Main,
+  decorators: [withKnobs]
+} as Meta
+
+export const Basic: Story = () => (
+  <Main
+    title={text('Title', 'React Avançado')}
+    description={text('Description', 'description padrão do boilerplate')}
+  />
+)
