483aa6730abdbdbcf769b9cb99a28067e0d214e6
LUCAS DUTRA <lucas.dutra@midway.com.br>
2021-04-26 16:03:01 -0300

    feature: adição do typescript

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,10 @@
 import Head from 'next/head'
 import styles from '../styles/Home.module.css'
 
+type Props = {
+  title: string;
+};
+
 export default function Home() {
   return (
     <div className={styles.container}>
