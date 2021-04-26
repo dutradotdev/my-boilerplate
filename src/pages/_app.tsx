49fa502848ce52403d55b19b90a8e0a4badec6e6
LUCAS DUTRA <lucas.dutra@midway.com.br>
2021-04-26 18:17:38 -0300

    feat: zerando estilos

---
+++ src/pages/_app.tsx
@@ -0,0 +1,21 @@
+import { AppProps } from 'next/app'
+import Head from 'next/head'
+
+import GlobalStyles from 'styles/global'
+
+function App({ Component, pageProps }: AppProps) {
+  return (
+    <>
+      <Head>
+        <title>Boilerplate</title>
+        <link rel="shortcut icon" href="/img/icon-512.png" />
+        <link rel="apple-touch-icon" href="/img/icon-512.png" />
+        <meta name="description" content="boilerplate" />
+      </Head>
+      <GlobalStyles />
+      <Component {...pageProps} />
+    </>
+  )
+}
+
+export default App
